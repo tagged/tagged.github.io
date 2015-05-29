050a2789fac032546050e5d8242051ee0ea94ebd
ryan <ryan@blackbox.alkanet>
2015-05-28 22:11:16 -0700

    [Change] Move files and tags to external resource files

---
+++ src/res/_files.js
@@ -0,0 +1,41 @@
+//Sample files
+module.exports = [{
+  name: "Getting Started With a Really really Really really Really really Long Title",
+  metadata: {
+    path: ["Dropbox","Samples"],
+    modified: "Modified 2015 Feb 28",
+    size: "25 KB",
+    type: "PDF",
+    cloud: "dropbox",
+    link: "//www.dropbox.com/home"
+  },
+  tags: ["pork","pork shoulder","honey","five spice","food.ingredients.sauces.oyster-sauce.lee-kum-kee.large-bbbottle","red bean paste", "shoyu"],
+  isOpen: false,
+  isChecked: false
+}, {
+  name: "pork ribs",
+  metadata: {
+    path: ["GoogleDrive","Samples","SmallDocuments","folder","subfolder","InnerFolder","CoreFolder"],
+    modified: "Modified 2014 Aug 12",
+    size: "2 KB",
+    type: "DOCX",
+    cloud: "google",
+    link: "//drive.google.com"
+  },
+  tags: ["pork","ribs","honey","vinegar"],
+  isOpen: false,
+  isChecked: false
+}, {
+  name: "Spicy pork bulgogi",
+  metadata: {
+    path: ["Dropbox","Samples"],
+    modified: "Modified 2014 Aug 12",
+    size: "2 KB",
+    type: "DOCX",
+    cloud: "dropbox",
+    link: "//dropbox.com/home"
+  },
+  tags: ["gochujang","gochugaru","pork","pork shoulder","spicy"],
+  isOpen: false,
+  isChecked: false
+}];
