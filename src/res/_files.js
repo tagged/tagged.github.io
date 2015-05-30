f17fdc23250e54cc0149acf6540f43a844c612cc
ryan <ryan@blackbox.alkanet>
2015-05-30 00:36:14 -0700

    [Change] Flatten sample file structure

--- src/res/_files.js
+++ src/res/_files.js
@@ -4,42 +4,36 @@ module.exports = [{
 
   id: 20123,
   name: "Getting Started With a Really really Really really Really really Long Title",
-  metadata: {
-    path: ["Dropbox","Samples"],
-    modified: "Modified 2015 Feb 28",
-    size: "25 KB",
-    type: "PDF",
-    cloud: "dropbox",
-    link: "//www.dropbox.com/home"
-  },
+  path: ["Dropbox","Samples"],
+  modified: "Modified 2015 Feb 28",
+  size: "25 KB",
+  type: "PDF",
+  cloud: "dropbox",
+  link: "//www.dropbox.com/home",
   tags: ["pork","pork shoulder","honey","five spice","food.ingredients.sauces.oyster-sauce.lee-kum-kee.large-bbbottle","red bean paste", "shoyu"],
 
 }, {
 
   id: 83748,
   name: "pork ribs",
-  metadata: {
-    path: ["GoogleDrive","Samples","SmallDocuments","folder","subfolder","InnerFolder","CoreFolder"],
-    modified: "Modified 2014 Aug 12",
-    size: "2 KB",
-    type: "DOCX",
-    cloud: "google",
-    link: "//drive.google.com"
-  },
+  path: ["GoogleDrive","Samples","SmallDocuments","folder","subfolder","InnerFolder","CoreFolder"],
+  modified: "Modified 2014 Aug 12",
+  size: "2 KB",
+  type: "DOCX",
+  cloud: "google",
+  link: "//drive.google.com",
   tags: ["pork","ribs","honey","vinegar"],
 
 }, {
 
   id: 37428,
   name: "Spicy pork bulgogi",
-  metadata: {
-    path: ["Dropbox","Samples"],
-    modified: "Modified 2014 Aug 12",
-    size: "2 KB",
-    type: "DOCX",
-    cloud: "dropbox",
-    link: "//dropbox.com/home"
-  },
+  path: ["Dropbox","Samples"],
+  modified: "Modified 2014 Aug 12",
+  size: "2 KB",
+  type: "DOCX",
+  cloud: "dropbox",
+  link: "//dropbox.com/home",
   tags: ["gochujang","gochugaru","pork","pork shoulder","spicy"],
 
 }];
