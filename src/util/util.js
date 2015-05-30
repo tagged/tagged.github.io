8c0adef9999d880ce4e423fe14785fdeb89547de
ryan <ryan@blackbox.alkanet>
2015-05-29 19:46:58 -0700

    [Change] Move path creation method into Util

--- src/util/util.js
+++ src/util/util.js
@@ -1,12 +1,18 @@
 module.exports = {
 
-  //Merge object2 into object1
-
   merge: function(object1, object2) {
+    //Merge object2 into object1
     for (var k in object2) {
       object1[k] = object2[k];
     }
     return object1;
+  },
+
+  makePath: function(pathArray) {
+    //Return a path string for a specified array of directory names
+    //String breaks on slashes
+    return pathArray.join(String.fromCharCode(8203) + "/");
   }
+
 
 }
