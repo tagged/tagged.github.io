a6af6f87fc8318fe4968feba6c1aea68478c5481
ryan <ryan@blackbox.alkanet>
2015-05-30 00:33:28 -0700

    [Change] Change prop handleClick to onClick

--- src/Tag.jsx
+++ src/Tag.jsx
@@ -2,14 +2,13 @@ var React = require('react/addons');
 var Color = require('./res/color');
 var Dimension = require('./res/dimension');
 var Typography = require('./res/typography');
-//var Velocity = require('../velocity/velocity.js');
 
 var Tag = React.createClass({
   
   propTypes: {
     text: React.PropTypes.string.isRequired,
     isDisabled: React.PropTypes.bool.isRequired,
-    handleClick: React.PropTypes.func.isRequired
+    onClick: React.PropTypes.func.isRequired
   },
 
   getStyle: function() {
@@ -45,7 +44,7 @@ var Tag = React.createClass({
 
   handleClick: function() {
     if (!this.props.isDisabled) {
-      this.props.handleClick();
+      this.props.onClick();
     }
   }
 
