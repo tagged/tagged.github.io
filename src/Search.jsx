0083aab0e7cbcb8ad683a01dcec6c27c95d5f1c4
ryan <ryan@blackbox.alkanet>
2015-06-01 14:53:56 -0700

    [Tidy] Reorganize render method of Search

--- src/Search.jsx
+++ src/Search.jsx
@@ -9,7 +9,7 @@ var Dimension = require('./res/dimension');
 var Search = React.createClass({
   
   propTypes: {
-    searchTags: React.PropTypes.array,
+    searchTags: React.PropTypes.array.isRequired,
     searchValue: React.PropTypes.string,
     searchIsFocused: React.PropTypes.bool,
     searchFiles: React.PropTypes.object,
@@ -28,83 +28,6 @@ var Search = React.createClass({
     onFileToggle: React.PropTypes.func,
   },
 
-  getTagInputProps: function() {
-    return {
-      value: this.props.searchValue,
-      isFocused: this.props.searchIsFocused,
-      handleFocus: this.props.onFocus,
-      handleBlur: this.props.onBlur,
-      handleChange: this.props.onChange,
-      placeholder: "Search files by tag",
-      maxWidth: 'none'
-    };
-  },
-  
-  getSearchTagNodes: function() {
-
-    return this.props.searchTags.map(function(tag, tagIndex) {
-
-      var onTagClick = function() {
-        this.props.onSearchTagDelete(tagIndex);
-      }.bind(this);
-      
-      return (
-        <Tag text={tag}
-             isDisabled={false}
-             onClick={onTagClick}
-             key={tag}/>
-      );
-
-    }, this);
- 
-  },
-
-  getSuggestionsNode: function(style) {
-    
-    if (!this.props.searchIsFocused && this.props.searchTags.length > 0) {
-      //If no search tags, even if not focused, show suggestions
-      return null;
-    }
-
-    var suggestedTagNodes = this.props.suggestedTags.map(function(tag) {
-
-      //Disable tag if it's already a search tag -- should always be false
-      var isDisabled = this.props.searchTags.indexOf(tag) >= 0;
-
-      var onTagClick = function() {
-        this.props.onSearchTagAdd(tag);
-      }.bind(this);
-
-      return (
-        <Tag text={tag}
-             isDisabled={isDisabled}
-             onClick={onTagClick}
-             key={tag}/>
-      );
-
-    }, this);
-
-    return (
-      <div>
-          <Subheader text={this.props.suggestionTitle}/>
-          <div style={style.suggestions}>
-              {suggestedTagNodes}
-          </div>
-      </div>
-    );
-
-  },
-
-  getFilesProps: function() {
-    return {
-      searchFiles: this.props.searchFiles,
-      onFileSelect: this.props.onFileSelect,
-      onFileToggle: this.props.onFileToggle,
-      disabledTags: this.props.searchTags,
-      onTagClick: this.props.onSearchTagAdd,
-    };
-  },
-
   getStyle: function() {
     return {
       search: {
@@ -121,14 +44,63 @@ var Search = React.createClass({
 
   render: function() {
     var style = this.getStyle();
+
+    var searchTags = this.props.searchTags.map(function(tag, tagIndex) {
+      var onTagClick = function() {
+        this.props.onSearchTagDelete(tagIndex);
+      }.bind(this);
+      return (
+        <Tag text={tag}
+             isDisabled={false}
+             onClick={onTagClick}
+             key={tag}/>
+      );
+    }, this);
+
+    var suggestions = null;
+    //Show suggestions if focused or if there are no search tags, even if not focused
+    if (this.props.searchIsFocused || this.props.searchTags.length === 0) {
+      var suggestedTags = this.props.suggestedTags.map(function(tag) {
+        //Disable tag if it's already a search tag
+        var isDisabled = this.props.searchTags.indexOf(tag) >= 0;//should always be false
+        var onTagClick = function() {
+          this.props.onSearchTagAdd(tag);
+        }.bind(this);
+        return (
+          <Tag text={tag}
+               isDisabled={isDisabled}
+               onClick={onTagClick}
+               key={tag}/>
+        );
+      }, this);
+      suggestions = (
+        <div>
+            <Subheader text={this.props.suggestionTitle}/>
+            <div style={style.suggestions}>
+                {suggestedTags}
+            </div>
+        </div>
+      );
+    }
+
     return (
       <div>
           <div style={style.search}>
-              {this.getSearchTagNodes()}
-              <TagInput {...this.getTagInputProps()}/>
-              {this.getSuggestionsNode(style)}
+              {searchTags}
+              <TagInput value={this.props.searchValue} 
+                        isFocused={this.props.searchIsFocused} 
+                        handleFocus={this.props.onFocus} 
+                        handleBlur={this.props.onBlur} 
+                        handleChange={this.props.onChange} 
+                        placeholder="Search files by tag"
+                        maxWidth="none"/>
+              {suggestions}
           </div>
-          <Files {...this.getFilesProps()}/>
+          <Files searchFiles={this.props.searchFiles}
+                 onFileSelect={this.props.onFileSelect}
+                 onFileToggle={this.props.onFileToggle}
+                 disabledTags={this.props.searchTags}
+                 onTagClick={this.props.onSearchTagAdd}/>
       </div>
     );
   }
