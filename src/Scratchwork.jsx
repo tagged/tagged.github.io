4f4c7cc19d41f9762606132b78933abfdd9a6b69
ryan <ryan@blackbox.alkanet>
2015-06-17 18:25:53 -0700

    [Change] Move scratchwork to its own component

---
+++ src/Scratchwork.jsx
@@ -0,0 +1,56 @@
+var React = require('react/addons');
+var MaterialIcon = require('./MaterialIcon');
+var ActionBar = require('./ActionBar');
+var Snackbar = require('./Snackbar');
+var R = require('./res/index');
+var Color = R.color;
+var Icon = R.icon;
+
+
+
+var Scratchwork = React.createClass({
+
+  render: function() {
+    var actionBarStyle = {
+      actionBar: {
+        position: 'fixed', bottom: 0, left:0, right: 0, zIndex: 99, 
+        backgroundColor: 'limegreen', 
+        paddingTop: 4, paddingBottom: 4,
+        paddingRight: 4, paddingLeft: 4,
+      },
+      menu: {menu: {bottom: 4, top:'auto'}},
+    };
+    return (
+      <div>
+          <MaterialIcon d={Icon.checkboxPartial}
+                        fill={Color.blue500} 
+                        fillOpacity={1}/>
+          
+          <Snackbar message="Hello, this is a really long message. What happens if a message is really long? Let's find out" 
+                    action="UNDO"
+                    onAction={function(){alert('Undone!')}}/>
+          
+          <ActionBar style={actionBarStyle}>
+              <div action="Blackify" style={{backgroundColor: 'black', width:48, height: 48}}/>
+              <div action="Grayify" style={{backgroundColor: 'gray', width:48, height: 48}}/>
+              <div action="Indigoify" style={{backgroundColor: 'indigo', width:48, height: 48}}/>
+              <div action="Purplify" style={{backgroundColor: 'purple', width:48, height: 48}}/>
+              <div action="Yellowify" style={{backgroundColor: 'yellow', width:48, height: 48}}/>
+              <div action="Orangify" style={{backgroundColor: 'orange', width:48, height: 48}}/>
+              <div action="Redify" style={{backgroundColor: 'red', width:48, height: 48}}/>
+              <div action="Pinkify" style={{backgroundColor: 'pink', width:48, height: 48}}/>
+              <div action="Blackify" style={{backgroundColor: 'black', width:48, height: 48}}/>
+              <div action="Grayify" style={{backgroundColor: 'gray', width:48, height: 48}}/>
+              <div action="Indigoify" style={{backgroundColor: 'indigo', width:48, height: 48}}/>
+              <div action="Purplify" style={{backgroundColor: 'purple', width:48, height: 48}}/>
+              <div action="Yellowify" style={{backgroundColor: 'yellow', width:48, height: 48}}/>
+              <div action="Orangify" style={{backgroundColor: 'orange', width:48, height: 48}}/>
+              <div action="Redify" style={{backgroundColor: 'red', width:48, height: 48}}/>
+              <div action="Pinkify" style={{backgroundColor: 'pink', width:48, height: 48}}/>
+          </ActionBar>
+      </div>
+    );
+  }
+});
+
+module.exports = Scratchwork;
--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ var Update = React.addons.update;
 
 var Search = require('./Search');
 var Cloud = require('./Cloud');
+var Scratchwork = require('./Scratchwork');
 
 var MaterialIcon = require('./MaterialIcon');
 var ActionBar = require('./ActionBar');
@@ -21,53 +22,6 @@ var Page = R.constant.page;
 
 var _Database = require('./res/_database');
 var Immutable = require('immutable');
-
-
-
-var Scratchwork = React.createClass({
-
-  render: function() {
-    var actionBarStyle = {
-      actionBar: {
-        position: 'fixed', bottom: 0, left:0, right: 0, zIndex: 99, 
-        backgroundColor: 'limegreen', 
-        paddingTop: 4, paddingBottom: 4,
-        paddingRight: 4, paddingLeft: 4,
-      },
-      menu: {menu: {bottom: 4, top:'auto'}},
-    };
-    return (
-      <div>
-          <MaterialIcon d={Icon.checkboxPartial}
-                        fill={Color.blue500} 
-                        fillOpacity={1}/>
-          
-          <Snackbar message="Hello, this is a really long message. What happens if a message is really long? Let's find out" 
-                    action="UNDO"
-                    onAction={function(){alert('Undone!')}}/>
-          
-          <ActionBar style={actionBarStyle}>
-              <div action="Blackify" style={{backgroundColor: 'black', width:48, height: 48}}/>
-              <div action="Grayify" style={{backgroundColor: 'gray', width:48, height: 48}}/>
-              <div action="Indigoify" style={{backgroundColor: 'indigo', width:48, height: 48}}/>
-              <div action="Purplify" style={{backgroundColor: 'purple', width:48, height: 48}}/>
-              <div action="Yellowify" style={{backgroundColor: 'yellow', width:48, height: 48}}/>
-              <div action="Orangify" style={{backgroundColor: 'orange', width:48, height: 48}}/>
-              <div action="Redify" style={{backgroundColor: 'red', width:48, height: 48}}/>
-              <div action="Pinkify" style={{backgroundColor: 'pink', width:48, height: 48}}/>
-              <div action="Blackify" style={{backgroundColor: 'black', width:48, height: 48}}/>
-              <div action="Grayify" style={{backgroundColor: 'gray', width:48, height: 48}}/>
-              <div action="Indigoify" style={{backgroundColor: 'indigo', width:48, height: 48}}/>
-              <div action="Purplify" style={{backgroundColor: 'purple', width:48, height: 48}}/>
-              <div action="Yellowify" style={{backgroundColor: 'yellow', width:48, height: 48}}/>
-              <div action="Orangify" style={{backgroundColor: 'orange', width:48, height: 48}}/>
-              <div action="Redify" style={{backgroundColor: 'red', width:48, height: 48}}/>
-              <div action="Pinkify" style={{backgroundColor: 'pink', width:48, height: 48}}/>
-          </ActionBar>
-      </div>
-    );
-  }
-});
 
 
 
@@ -669,8 +623,5 @@ var App = React.createClass({
   }
 
 });
-
-
-
 
 React.render(<App/>, document.getElementById('content'));
