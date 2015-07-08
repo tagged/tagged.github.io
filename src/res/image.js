b8946434c97740ee4f55ac767a8ddb3db4373d2b
ryan <ryan@blackbox.alkanet>
2015-07-08 14:57:30 -0700

    [Dev] Move images to image/

--- src/res/image.js
+++ src/res/image.js
@@ -1,14 +1,14 @@
 module.exports = {
   'Dropbox': {
-    src: '/dropbox-logo-128.png',
+    src: '/image/dropbox-logo-128.png',
     alt: 'Dropbox Logo'
   },
   'Google Drive': {
-    src: '/google-drive-logo-128.png',
+    src: '/image/google-drive-logo-128.png',
     alt: 'Google Drive Logo'
   },
   'Box': {
-    src: '/box-logo-128.png',
+    src: '/image/box-logo-128.png',
     alt: 'Box Logo'
   }
 };
